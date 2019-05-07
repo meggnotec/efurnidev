930abd9fa9addbd70197c0398d653fff14915493
Vignesh <chandramohan@meggnotec.com>
2019-05-07 13:45:21 +0530

    2nd commit on dev path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,21 +6,21 @@ source $NVM_DIR/nvm.sh
 if [ "$1" == "bash" ]; then
 	bash
 elif [ "$1" == "start" ]; then
-	if [ ! -e "/bcstencil/efurni/package.json" ]; then
-	    if [ ! "$(ls -A /bcstencil/efurni)" ]; then
-		    git clone git://github.com/bigcommerce/cornerstone.git /bcstencil/efurni
+	if [ ! -e "/bcstencil/efurni/efurnidev/efurnitheme/package.json" ]; then
+	    if [ ! "$(ls -A /bcstencil/efurni/efurnidev/efurnitheme)" ]; then
+		    git clone git://github.com/bigcommerce/cornerstone.git /bcstencil/efurni/efurnidev/efurnitheme
 	    else
-	      echo "/bcstencil/efurni must either be empty, or hold a valid stencil theme"
+	      echo "/bcstencil/efurni/efurnidev/efurnitheme must either be empty, or hold a valid stencil theme"
       fi
 	fi
 
-	if [ ! -e "/bcstencil/efurni/node_modules" ]; then
+	if [ ! -e "/bcstencil/efurni/efurnidev/efurnitheme/node_modules" ]; then
 		npm install --loglevel=error
 	fi
 
-	if [ ! -e "/bcstencil/efurni/.stencil" ]; then
+	if [ ! -e "/bcstencil/efurni/efurnidev/efurnitheme/.stencil" ]; then
 		if [ -e "/etc/stencil/.stencil" ]; then
-			cp /etc/stencil/.stencil /bcstencil/efurni/
+			cp /etc/stencil/.stencil /bcstencil/efurni/efurnidev/efurnitheme
 		else
 			stencil init
 		fi
